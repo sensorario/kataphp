9794494c85dcd785de9d9d3f51c522435dae9988
sensorario <sensorario@gmail.com>
2014-01-22 13:39:56 +0100

    La classe PaperRockScissors esiste

---
+++ Tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+spl_autoload_register(
+    function ($class) {
+        $require = __DIR__ . '/../' . str_replace("\\", '/', $class) . '.php';
+        if (file_exists($require)) {
+            require_once($require);
+        }
+    }
+);
