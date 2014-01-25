91b07080b5c8980f04f97e225172875ce51e085f
sensorario <sensorario@gmail.com>
2014-01-25 03:42:17 +0100

    SImpathetic and nightly try ...

---
+++ PaperRockScissors/ThirdTry/Player.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PaperRockScissors\ThirdTry;
+
+class Player
+{
+    const CHOICE_PAPER = 'paper';
+    const CHOICE_ROCK = 'rock';
+    const CHOICE_SCISSORS = 'scissors';
+
+    private $choice;
+
+    public function __construct($choice)
+    {
+        $this->choice = $choice;
+    }
+
+    public function buildFromChoice($choice)
+    {
+        return new Player($choice);
+    }
+
+    public function getChoice()
+    {
+        return $this->choice;
+    }
+
+    public function winVersus(Player $player)
+    {
+        return $this->choice == Player::CHOICE_SCISSORS && $player->choice == Player::CHOICE_PAPER
+        || $this->choice == Player::CHOICE_PAPER && $player->choice == Player::CHOICE_ROCK
+        || $this->choice == Player::CHOICE_ROCK && $player->choice == Player::CHOICE_SCISSORS;
+    }
+}
---
+++ Tests/PaperRockScissors/ThirdTry/PlayerFactoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\PaperRockScissors\ThirdTry;
+
+use PaperRockScissors\ThirdTry\Player;
+
+class PlayerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPlayerEntity()
+    {
+        $tizio = Player::buildFromChoice(Player::CHOICE_PAPER);
+        $this->assertEquals(get_class($tizio), 'PaperRockScissors\ThirdTry\Player');
+    }
+
+    public function testPlayerEntityReturnsChoice()
+    {
+        $tizio = Player::buildFromChoice(Player::CHOICE_PAPER);
+        $this->assertEquals(Player::CHOICE_PAPER, $tizio->getChoice());
+    }
+
+    /**
+     * @dataProvider winMatrix
+     */
+    public function testMatch($first, $second, $expect)
+    {
+        $tizio = Player::buildFromChoice($first);
+        $caio = Player::buildFromChoice($second);
+        $this->assertEquals($expect, $tizio->winVersus($caio));
+    }
+
+    public function winMatrix()
+    {
+        return [
+            [Player::CHOICE_PAPER, Player::CHOICE_ROCK, true],
+            [Player::CHOICE_ROCK, Player::CHOICE_SCISSORS, true],
+            [Player::CHOICE_SCISSORS, Player::CHOICE_PAPER, true],
+            [Player::CHOICE_SCISSORS, Player::CHOICE_ROCK, false],
+            [Player::CHOICE_ROCK, Player::CHOICE_PAPER, false],
+            [Player::CHOICE_PAPER, Player::CHOICE_SCISSORS, false],
+        ];
+    }
+}
