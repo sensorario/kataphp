4f98adee65b1a591cff0a56460c559e5cf3d8dbc
sensorario <sensorario@gmail.com>
2014-01-30 00:53:58 +0100

    hasPoints

--- Bowling/Implementation01/Player.php
+++ Bowling/Implementation01/Player.php
@@ -8,40 +8,37 @@ use Bowling\Implementation01\PlayerCanLaunchOneTimeException;
 class Player
 {
     private $points;
-    private $launch;
+    private $turns;
     private $launchDone = 0;
 
     public function __construct()
     {
         $this->points = 0;
-        $this->launch = array();
+        $this->turns = array();
         $this->twoLaunchAvailable = false;
     }
 
     public function hasPoints()
     {
         $sumPoints = 0;
-        for ($i = 0; $i < count($this->launch); $i++) {
-            $currentLaunch = $this->launch[$i];
-            if ($i > 0) {
-                $previousLaunch = $this->launch[$i - 1];
-                $sumPoints += $currentLaunch + $previousLaunch;
-            } else {
-                $sumPoints += $currentLaunch;
-            }
+
+        for ($i = 0; $i < count($this->turns); $i++) {
+            $currentLaunch = $this->turns[$i];
+            $sumPoints += $currentLaunch;
         }
+
         return $sumPoints;
     }
 
     public function doStrike()
     {
-        $countLaunch = count($this->launch);
+        $countLaunch = count($this->turns);
 
         if ($countLaunch != 0) {
             throw new PlayerCanLaunchOneTimeException;
         }
 
-        $this->launch[$countLaunch] = 10;
+        $this->turns[$countLaunch] = 10;
     }
 
     public function doLaunch()
