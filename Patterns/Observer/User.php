f267a0527ad31fcdab26e56ec0d80720922843e4
sensorario <sensorario@gmail.com>
2014-02-25 05:56:17 +0100

    Created observable class

--- Patterns/Observer/User.php
+++ Patterns/Observer/User.php
@@ -2,7 +2,22 @@
 
 namespace Patterns\Observer;
 
-class User
+use SplSubject;
+
+class User implements \SplObserver
 {
 
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Receive update from subject
+     * @link http://php.net/manual/en/splobserver.update.php
+     * @param SplSubject $subject <p>
+     * The <b>SplSubject</b> notifying the observer of an update.
+     * </p>
+     * @return void
+     */
+    public function update(SplSubject $subject)
+    {
+        // TODO: Implement update() method.
+    }
 }
--- Tests/Patterns/Observer/UserTest.php
+++ Tests/Patterns/Observer/UserTest.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace Patterns\Observer;
+namespace Tests\Patterns\Observer;
 
 use Patterns\Observer\User;
+use PHPUnit_Framework_TestCase;
 
-class UserTest
+class UserTest extends PHPUnit_Framework_TestCase
 {
     public function testUser()
     {
         $user = new User();
+        $this->assertInstanceOf('\SplObserver', $user);
     }
 }
