35ddbd0af7753f9e6eaca92d7a60eca5791a624a
sensorario <sensorario@gmail.com>
2014-02-25 06:14:59 +0100

    Added Observer pattern

--- Patterns/Observer/User.php
+++ Patterns/Observer/User.php
@@ -2,10 +2,24 @@
 
 namespace Patterns\Observer;
 
+use ArrayObject;
+use SplObserver;
 use SplSubject;
 
-class User implements \SplObserver
+class User implements SplObserver, SplSubject
 {
+    /**
+     * @var ArrayObject
+     */
+    private $observers;
+
+    private $username;
+
+    public function __construct($username)
+    {
+        $this->username = $username;
+        $this->observers = [];
+    }
 
     /**
      * (PHP 5 &gt;= 5.1.0)<br/>
@@ -19,5 +33,56 @@ class User implements \SplObserver
     public function update(SplSubject $subject)
     {
         // TODO: Implement update() method.
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Attach an SplObserver
+     * @link http://php.net/manual/en/splsubject.attach.php
+     * @param SplObserver $observer <p>
+     * The <b>SplObserver</b> to attach.
+     * </p>
+     * @return void
+     */
+    public function attach(SplObserver $observer)
+    {
+        $this->observers[] = $observer;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Detach an observer
+     * @link http://php.net/manual/en/splsubject.detach.php
+     * @param SplObserver $observer <p>
+     * The <b>SplObserver</b> to detach.
+     * </p>
+     * @return void
+     */
+    public function detach(SplObserver $observer)
+    {
+        // TODO: Implement detach() method.
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Notify an observer
+     * @link http://php.net/manual/en/splsubject.notify.php
+     * @return void
+     */
+    public function notify()
+    {
+        foreach ($this->observers as $observer) {
+            echo $observer->getUsername();
+        }
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getObservers()
+    {
+        return $this->observers;
     }
 }
--- Tests/Patterns/Observer/UserTest.php
+++ Tests/Patterns/Observer/UserTest.php
@@ -9,7 +9,36 @@ class UserTest extends PHPUnit_Framework_TestCase
 {
     public function testUser()
     {
-        $user = new User();
+        $user = new User('simone');
+
         $this->assertInstanceOf('\SplObserver', $user);
+        $this->assertInstanceOf('\SplSubject', $user);
+    }
+
+    public function testGetter()
+    {
+        $user = new User('pippo');
+        $this->assertEquals('pippo', $user->getUsername());
+    }
+
+    public function testObserver()
+    {
+        $simone = new User('simone');
+        $marco = new User('marco');
+        $andrea = new User('andrea');
+        $cesare = new User('cesare');
+
+        $cesare->attach($simone);
+        $cesare->attach($marco);
+        $this->assertEquals(
+            [$simone, $marco],
+            $cesare->getObservers()
+        );
+
+        $andrea->attach($cesare);
+        $this->assertEquals(
+            [$cesare],
+            $andrea->getObservers()
+        );
     }
 }
