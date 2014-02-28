44a7d2631a8077b8a7619188f1a6e2b5d50e3982
sensorario <sensorario@gmail.com>
2014-02-28 11:00:20 +0100

    Added a learning test for interval

--- Tests/DateTime/DateTimeTest.php
+++ Tests/DateTime/DateTimeTest.php
@@ -11,4 +11,13 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
         $dateTime = new DateTime("May 30, 1980");
         $this->assertEquals("30/05/1980", $dateTime->format("d/m/Y"));
     }
+
+    public function testAddInterval()
+    {
+        $dateTime = new DateTime("May 5, 1953");
+        $intervalSpec = "P7D";
+        $interval = new \DateInterval($intervalSpec);
+        $dateTime->add($interval);
+        $this->assertEquals("12/05/1953", $dateTime->format("d/m/Y"));
+    }
 }
