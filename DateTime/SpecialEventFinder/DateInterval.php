744e338103522bb8f4ea2b603fc88a59af40091b
sensorario <sensorario@gmail.com>
2014-03-03 09:31:39 +0100

    DateInterval

---
+++ DateTime/SpecialEventFinder/DateInterval.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace DateTime\SpecialEventFinder;
+
+use DateTime;
+
+class DateInterval
+{
+    /**
+     * @var DateTime
+     */
+    private $dateFrom;
+
+    /**
+     * @var DateTime
+     */
+    private $dateTo;
+
+    public function __construct()
+    {
+        $this->dateFrom = new DateTime("-1 days");
+        $this->dateTo = new DateTime("+6 days");
+    }
+
+    public function setDateFrom(DateTime $from)
+    {
+        $this->dateFrom = $from;
+    }
+
+    public function setDateTo(DateTime $to)
+    {
+        $this->dateTo = $to;
+    }
+
+    public function daysRangeLength()
+    {
+        return $this->dateTo->diff($this->dateFrom)->days;
+    }
+
+    public function getDateFrom()
+    {
+        return $this->dateFrom;
+    }
+
+    public function getDateTo()
+    {
+        return $this->dateTo;
+    }
+
+    public function contains(DateTime $dateTime)
+    {
+        if ($dateTime > $this->dateTo || $dateTime < $this->dateFrom) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Tests/DateTime/SpecialEventFinder/DateIntervalTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\DateTime\SpecialEventFinder;
+
+use DateTime\SpecialEventFinder\DateInterval;
+use DateTime;
+use PHPUnit_Framework_TestCase;
+
+class DateIntervalTest extends PHPUnit_Framework_TestCase
+{
+    public function testDatesRange()
+    {
+        $finder = new DateInterval();
+        $finder->setDateFrom(new DateTime("-3 days"));
+        $finder->setDateTo(new DateTime("+4 days"));
+        $this->assertEquals(7, $finder->daysRangeLength());
+    }
+
+    public function testRangeStartsFromYesterday()
+    {
+        $defaultRange = new DateInterval();
+        $this->assertEquals($defaultRange->getDateFrom(), new DateTime("-1 days"));
+    }
+
+    public function testRangeEndsToSixDays()
+    {
+        $defaultRange = new DateInterval();
+        $this->assertEquals($defaultRange->getDateTo(), new DateTime("+6 days"));
+    }
+
+    /**
+     * @dataProvider datesInside
+     */
+    public function testDateIsInsideInterval(DateTime $dateTime)
+    {
+        $interval = new DateInterval();
+        $this->assertTrue($interval->contains($dateTime));
+    }
+
+    public function datesInside()
+    {
+        return [
+            [new DateTime("-1 days")],
+            [new DateTime()],
+            [new DateTime("+1 days")],
+            [new DateTime("+2 days")],
+            [new DateTime("+3 days")],
+            [new DateTime("+4 days")],
+            [new DateTime("+5 days")],
+            [new DateTime("+6 days")],
+        ];
+    }
+
+    /**
+     * @dataProvider datesOutside
+     */
+    public function testDateIsNotInsideInterval(DateTime $date)
+    {
+        $interval = new DateInterval();
+        $this->assertFalse($interval->contains($date));
+    }
+
+    public function datesOutside()
+    {
+        return [
+            [new DateTime("+33 days")],
+            [new DateTime("-11 days")],
+        ];
+    }
+}
