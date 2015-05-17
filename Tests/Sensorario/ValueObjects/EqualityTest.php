adbbec4292d03ac443efb1060239111193974718
Simone Gentili <simone.gentili@neomobile.com>
2015-05-18 00:43:46 +0200

    Removed useless use, and refactored test class of Speaker Value Object

--- Tests/Sensorario/ValueObjects/EqualityTest.php
+++ Tests/Sensorario/ValueObjects/EqualityTest.php
@@ -2,55 +2,44 @@
 namespace Sensorario\ValueObjects;
 
 use PHPUnit_Framework_TestCase;
-use Sensorario\ValueObjects\IntegerValue;
-use Sensorario\ValueObjects\StringLiteral;
 
 class SpeakerTest extends PHPUnit_Framework_TestCase
 {
-    public function testSimoneAndHisTwinAreEquals()
+    public function setUp()
     {
-        $simone = new Speaker(
+        $this->simone = new Speaker(
             new StringLiteral("Simone"),
             new StringLiteral("Gentili"),
             new IntegerValue("32")
         );
+    }
 
+    public function testSimoneAndHisTwinAreEquals()
+    {
         $twin = new Speaker(
             new StringLiteral("Simone"),
             new StringLiteral("Gentili"),
             new IntegerValue("32")
         );
 
-        $this->assertTrue($simone == $twin);
+        $this->assertTrue($this->simone == $twin);
     }
 
     public function testTwiObjectAreNotEquals()
     {
-        $simone = new Speaker(
-            new StringLiteral("Simone"),
-            new StringLiteral("Gentili"),
-            new IntegerValue("32")
-        );
-
         $davide = new Speaker(
             new StringLiteral("Davide"),
             new StringLiteral("Bellettini"),
             new IntegerValue("29")
         );
 
-        $this->assertFalse($simone == $davide);
+        $this->assertFalse($this->simone == $davide);
     }
 
     public function testGetters()
     {
-        $simone = new Speaker(
-            new StringLiteral("Simone"),
-            new StringLiteral("Gentili"),
-            new IntegerValue("32")
-        );
-
-        $this->assertEquals("Simone",  $simone->name());
-        $this->assertEquals("Gentili", $simone->surname());
-        $this->assertEquals(32,        $simone->age());
+        $this->assertEquals("Simone",  $this->simone->name());
+        $this->assertEquals("Gentili", $this->simone->surname());
+        $this->assertEquals(32,        $this->simone->age());
     }
 }
